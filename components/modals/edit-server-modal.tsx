f5e890178a330b080aea2c06271542cf8337ecd3
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2025-02-04 17:30:11 -0700

    Added edit server functionality and modal

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 import {
     Dialog,
@@ -29,6 +30,7 @@ import { FileUpload } from "@/components/file-upload";
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
 
+
 const formSchema = z.object({
     name: z.string().min(1, {
         message: "Server name is required."
@@ -39,11 +41,12 @@ const formSchema = z.object({
 })
 
 // checks to make sure the form has proper submissions
-export const CreateServerModal = () => {
-    const { isOpen, onClose, type } = useModal();
+export const EditServerModal = () => {
+    const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
 
-    const isModalOpen = isOpen && type === "createServer";
+    const isModalOpen = isOpen && type === "editServer";
+    const { server } = data;
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -53,12 +56,19 @@ export const CreateServerModal = () => {
         }
     });
 
+    useEffect(() => {
+        if (server) {
+            form.setValue("name", server.name)
+            form.setValue("imageUrl", server.imageUrl)
+        }
+    }, [server, form])
+
     //checks to make sure more submissions can't be made while a submission is already being made
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            await axios.post("/api/servers", values)
+            await axios.patch(`/api/servers/${server?.id}`, values)
 
             form.reset();
             router.refresh();
@@ -133,7 +143,7 @@ export const CreateServerModal = () => {
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                             <Button variant="primary" disabled={isLoading}>
-                                Create
+                                Save
                             </Button>
                         </DialogFooter>
                     </form>
