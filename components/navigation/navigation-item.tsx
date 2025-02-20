dc7a82c35084449ca34de20bce49bdf3ce8535f9
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2025-02-20 00:48:53 -0700

    Implemented initial code for channel page viewing.

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -21,7 +21,7 @@ export const NavigationItem = ({
     const router = useRouter();
 
     const onClick = () => {
-        router.push(`/servers/${id}}`)
+        router.push(`/servers/${id}`)
     }
 
     return (
