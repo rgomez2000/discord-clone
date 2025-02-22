7cf9c62e64eb600c43877e8a3621e8297ec4b55a
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2025-02-22 02:39:16 -0700

    Implemented SocketIO's websockets for improved query handling

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
 
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -41,6 +42,9 @@ export const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`
+
 
     const {  
         data,
@@ -57,6 +61,8 @@ export const ChatMessages = ({
         paramKey,
         paramValue,
     });
+    useChatSocket({ queryKey, addKey, updateKey })
+
 
     if (isLoading) {
         return (
